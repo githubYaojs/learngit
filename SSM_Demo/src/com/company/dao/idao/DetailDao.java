e4901dfe288d38956699545a3501549a8d3d32e7
yy <474569304@qq.com>
2017-11-18 13:49:26 +0800

    v1

--- SSM_Demo/src/com/company/dao/idao/DetailDao.java
+++
@@ -1,7 +0,0 @@
-package com.company.dao.idao;
-
-import com.company.dao.pojo.Detail;
-
-public interface DetailDao extends BaseDao<Detail, Integer> {
-
-}
--- SSM_Demo/src/com/company/service/iservice/DeptService.java
+++
@@ -1,7 +0,0 @@
-package com.company.service.iservice;
-
-import com.company.dao.pojo.Dept;
-
-public interface DeptService extends BaseService<Dept, Integer> {
-
-}
