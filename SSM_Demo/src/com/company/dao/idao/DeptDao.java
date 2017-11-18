cdb0e33a4f208e60369327930d4172cdd3c8b18e
yy <474569304@qq.com>
2017-11-18 13:35:58 +0800

    version1

--- SSM_Demo/src/com/company/dao/idao/DeptDao.java
+++
@@ -1,7 +0,0 @@
-package com.company.dao.idao;
-
-import com.company.dao.pojo.Dept;
-
-public interface DeptDao extends BaseDao<Dept, Integer> {
-
-}
--- SSM_Demo/src/com/company/service/iservice/ReportService.java
+++
@@ -1,6 +0,0 @@
-package com.company.service.iservice;
-
-public interface ReportService {
-	String reportDeptInfo();//前后端分离的情况下，返回的是json字符串
-	String reportAvgSalByYear();
-}
